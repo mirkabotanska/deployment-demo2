078da8d0c00c61ef047c9fa6395b9647c68e0956
mirka <miriam.botanska@student.howest.be>
2025-11-19 11:54:04 +0100

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://your-github-username.github.io',
-    base: '/cck/',
+    site: 'https://mirkabotanska.github.io',
+    base: '/deployment-demo2/',
 });
